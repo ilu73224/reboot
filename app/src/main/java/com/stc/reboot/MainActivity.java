2392dfc4d27b96723099b5b6d22cfa370f6262d9
ilu73224 <ilu73224@gmail.com>
2017-03-26 23:16:17 +0800

    change application action from just warm reboot to  show system reboot/shutdown dialog by long power key press event.

--- app/src/main/java/com/stc/reboot/MainActivity.java
+++ app/src/main/java/com/stc/reboot/MainActivity.java
@@ -2,19 +2,56 @@ package com.stc.reboot;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainActivity extends Activity {
-    private Process p=null;
+    public static final String TAG = "reboot";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        tryReboot();
+        finish();
+    }
+
+    public void tryReboot() {
         try {
-            p = Runtime.getRuntime().exec("su shell setprop ctl.restart zygote");
-        } catch (IOException e) {
+            Process pReboot = Runtime.getRuntime().exec("su");
+            DataOutputStream opt = new DataOutputStream(pReboot.getOutputStream());
+            opt.writeBytes("sendevent /dev/input/event2 1 116 1\n");
+            opt.writeBytes("sendevent /dev/input/event2 0 0 0\n");
+            opt.writeBytes("sleep 3\n");
+            opt.writeBytes("sendevent /dev/input/event2 1 116 0\n");
+            opt.writeBytes("sendevent /dev/input/event2 0 0 0\n");
+            opt.writeBytes("exit\n");
+            opt.flush();
+            pReboot.waitFor();
+            if(0 == pReboot.exitValue()) {
+                BufferedReader bufferedReader = new BufferedReader(
+                        new InputStreamReader(pReboot.getInputStream()));
+                StringBuilder log = new StringBuilder();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    log.append(String.format("%s\n", line));
+                }
+                Log.d(TAG, "run success result = \n " + log.toString());
+            } else {
+                Log.d(TAG, "run failed exit value =  " + pReboot.exitValue());
+                BufferedReader bufferedReader = new BufferedReader(
+                        new InputStreamReader(pReboot.getErrorStream()));
+                StringBuilder log = new StringBuilder();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    log.append(String.format("%s\n", line));
+                }
+                Log.d(TAG, "Error = \n " + log.toString());
+            }
+        } catch (InterruptedException | IOException e) {
             e.printStackTrace();
         }
-        finish();
     }
 }
